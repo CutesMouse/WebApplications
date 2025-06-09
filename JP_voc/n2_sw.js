ddd33ecd4b7dbb0ec9ec71d3524e8b898ef69a65
CutesMouse <57491074+CutesMouse@users.noreply.github.com>
2025-06-09 22:17:32 +0800

    更新 - N2支援PWA

---
+++ JP_voc/n2_sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'mouse-n2voc-20250609'; // ← 每次改這個版本號就好
+
+const urlsToCache = [
+    './',
+    './index_n2.html',
+    './index_n2.css',
+    './Control.js',
+    './Favorite.js',
+    './JapaneseGraphic.js',
+    './Question.js',
+    './Voc.js',
+    './Voc_List_n2.js',
+    './VocCard.js',
+    './icons/n2-icon-512.png',
+    './icons/n2-icon-192.png'
+];
+
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames.map(name => {
+                if (name !== CACHE_NAME) {
+                    return caches.delete(name);
+                }
+            })
+        ))
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response =>
+            response || fetch(event.request)
+        )
+    );
+});
